3c311554c5f288e3d049c5efb1c049aa1beff4b9
yunmikun <a.paramonenko@elonsoft.ru>
2021-08-16 20:46:53 +0300

    feat: Add /suggest/address request support

--- lib/ex_dadata/address.ex
+++ lib/ex_dadata/address.ex
@@ -5,7 +5,7 @@ defmodule ExDadata.Address do
   For more information see [docs](https://dadata.ru/api/#address-clean).
   """
 
-  alias __MODULE__.CleanAddress
+  alias __MODULE__.{CleanAddress, SuggestAddress}
   alias ExDadata.Client
   alias ExDadata.HTTPAdapter.Response
 
@@ -22,6 +22,31 @@ defmodule ExDadata.Address do
     adapter = Client.adapter(client)
 
     case adapter.request(:post, @clean_address_url, headers, data, []) do
+      {:ok, %Response{status: 200, body: response}} -> {:ok, response}
+      {:ok, resp} -> {:error, resp}
+      {:error, _} = error -> error
+    end
+  end
+
+  @suggest_address_url "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address"
+
+  def suggest_address(client, data) do
+    with {:ok, body} <- do_suggest_address(client, data) do
+      {:ok, SuggestAddress.new!(body)}
+    end
+  end
+
+  defp do_suggest_address(client, data) do
+    headers =
+      Client.to_headers(client) ++
+        [
+          {"Accept", "application/json"},
+          {"Content-Type", "application/json"}
+        ]
+
+    adapter = Client.adapter(client)
+
+    case adapter.request(:post, @suggest_address_url, headers, data, []) do
       {:ok, %Response{status: 200, body: response}} -> {:ok, response}
       {:ok, resp} -> {:error, resp}
       {:error, _} = error -> error
--- test/ex_dadata/address_test.exs
+++ test/ex_dadata/address_test.exs
@@ -4,7 +4,7 @@ defmodule ExDadata.AddressTest do
   alias ExDadata.{Address, Client}
 
   describe "clean_address/2" do
-    defmodule MockAddressHTTPAdapter do
+    defmodule MockCleanAddressHTTPAdapter do
       @behaviour ExDadata.HTTPAdapter
 
       alias ExDadata.HTTPAdapter.Response
@@ -32,7 +32,7 @@ defmodule ExDadata.AddressTest do
     end
 
     test "returns addresses on success" do
-      client = Client.new(nil, nil, MockAddressHTTPAdapter)
+      client = Client.new(nil, nil, MockCleanAddressHTTPAdapter)
       assert {:ok, %{items: [item]}} = Address.clean_address(client, [""])
 
       assert %{
@@ -49,6 +49,208 @@ defmodule ExDadata.AddressTest do
                geo_lon: 37.65372,
                qc_geo: 0
              } = item
+    end
+  end
+
+  describe "suggest_address/2" do
+    defmodule MockSuggestAddressHTTPAdapter do
+      @behaviour ExDadata.HTTPAdapter
+
+      alias ExDadata.HTTPAdapter.Response
+
+      @url "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address"
+
+      def request(:post, @url, _, _, _) do
+        body = %{
+          "suggestions" => [
+            %{
+              "value" => "г Москва, ул Хабаровская",
+              "unrestricted_value" => "г Москва, ул Хабаровская",
+              "data" => %{
+                "postal_code" => nil,
+                "country" => "Россия",
+                "country_iso_code" => "RU",
+                "federal_district" => nil,
+                "region_fias_id" => "0c5b2444-70a0-4932-980c-b4dc0d3f02b5",
+                "region_kladr_id" => "7700000000000",
+                "region_iso_code" => "RU-MOW",
+                "region_with_type" => "г Москва",
+                "region_type" => "г",
+                "region_type_full" => "город",
+                "region" => "Москва",
+                "area_fias_id" => nil,
+                "area_kladr_id" => nil,
+                "area_with_type" => nil,
+                "area_type" => nil,
+                "area_type_full" => nil,
+                "area" => nil,
+                "city_fias_id" => "0c5b2444-70a0-4932-980c-b4dc0d3f02b5",
+                "city_kladr_id" => "7700000000000",
+                "city_with_type" => "г Москва",
+                "city_type" => "г",
+                "city_type_full" => "город",
+                "city" => "Москва",
+                "city_area" => nil,
+                "city_district_fias_id" => nil,
+                "city_district_kladr_id" => nil,
+                "city_district_with_type" => nil,
+                "city_district_type" => nil,
+                "city_district_type_full" => nil,
+                "city_district" => nil,
+                "settlement_fias_id" => nil,
+                "settlement_kladr_id" => nil,
+                "settlement_with_type" => nil,
+                "settlement_type" => nil,
+                "settlement_type_full" => nil,
+                "settlement" => nil,
+                "street_fias_id" => "32fcb102-2a50-44c9-a00e-806420f448ea",
+                "street_kladr_id" => "77000000000713400",
+                "street_with_type" => "ул Хабаровская",
+                "street_type" => "ул",
+                "street_type_full" => "улица",
+                "street" => "Хабаровская",
+                "house_fias_id" => nil,
+                "house_kladr_id" => nil,
+                "house_cadnum" => nil,
+                "house_type" => nil,
+                "house_type_full" => nil,
+                "house" => nil,
+                "block_type" => nil,
+                "block_type_full" => nil,
+                "block" => nil,
+                "flat_fias_id" => nil,
+                "flat_cadnum" => nil,
+                "flat_type" => nil,
+                "flat_type_full" => nil,
+                "flat" => nil,
+                "flat_area" => nil,
+                "square_meter_price" => nil,
+                "flat_price" => nil,
+                "postal_box" => nil,
+                "fias_id" => "32fcb102-2a50-44c9-a00e-806420f448ea",
+                "fias_code" => nil,
+                "fias_level" => "7",
+                "fias_actuality_state" => nil,
+                "kladr_id" => "77000000000713400",
+                "geoname_id" => nil,
+                "capital_marker" => "0",
+                "okato" => "45263564000",
+                "oktmo" => "45305000",
+                "tax_office" => "7718",
+                "tax_office_legal" => "7718",
+                "timezone" => nil,
+                "geo_lat" => "55.8782557",
+                "geo_lon" => "37.65372",
+                "beltway_hit" => nil,
+                "beltway_distance" => nil,
+                "metro" => nil,
+                "qc_geo" => nil,
+                "qc_complete" => nil,
+                "qc_house" => nil,
+                "history_values" => [
+                  "ул Черненко"
+                ],
+                "unparsed_parts" => nil,
+                "source" => nil,
+                "qc" => nil
+              }
+            }
+          ]
+        }
+
+        {:ok, %Response{status: 200, headers: [], body: body}}
+      end
+    end
+
+    test "returns suggestion on success" do
+      client = Client.new(nil, nil, MockSuggestAddressHTTPAdapter)
+
+      assert {:ok, %{suggestions: [suggestion]}} =
+               Address.suggest_address(client, %{query: "москва хабар"})
+
+      assert %{
+               value: "г Москва, ул Хабаровская",
+               unrestricted_value: "г Москва, ул Хабаровская",
+               data: %{
+                 postal_code: nil,
+                 country: "Россия",
+                 country_iso_code: "RU",
+                 federal_district: nil,
+                 region_fias_id: "0c5b2444-70a0-4932-980c-b4dc0d3f02b5",
+                 region_kladr_id: "7700000000000",
+                 region_iso_code: "RU-MOW",
+                 region_with_type: "г Москва",
+                 region_type: "г",
+                 region_type_full: "город",
+                 region: "Москва",
+                 area_fias_id: nil,
+                 area_kladr_id: nil,
+                 area_with_type: nil,
+                 area_type: nil,
+                 area_type_full: nil,
+                 area: nil,
+                 city_fias_id: "0c5b2444-70a0-4932-980c-b4dc0d3f02b5",
+                 city_kladr_id: "7700000000000",
+                 city_with_type: "г Москва",
+                 city_type: "г",
+                 city_type_full: "город",
+                 city: "Москва",
+                 city_area: nil,
+                 city_district_fias_id: nil,
+                 city_district_kladr_id: nil,
+                 city_district_with_type: nil,
+                 city_district_type: nil,
+                 city_district_type_full: nil,
+                 city_district: nil,
+                 settlement_fias_id: nil,
+                 settlement_kladr_id: nil,
+                 settlement_with_type: nil,
+                 settlement_type: nil,
+                 settlement_type_full: nil,
+                 settlement: nil,
+                 street_fias_id: "32fcb102-2a50-44c9-a00e-806420f448ea",
+                 street_kladr_id: "77000000000713400",
+                 street_with_type: "ул Хабаровская",
+                 street_type: "ул",
+                 street_type_full: "улица",
+                 street: "Хабаровская",
+                 house_fias_id: nil,
+                 house_kladr_id: nil,
+                 house_type: nil,
+                 house_type_full: nil,
+                 house: nil,
+                 block_type: nil,
+                 block_type_full: nil,
+                 block: nil,
+                 flat_fias_id: nil,
+                 flat_type: nil,
+                 flat_type_full: nil,
+                 flat: nil,
+                 flat_area: nil,
+                 square_meter_price: nil,
+                 flat_price: nil,
+                 postal_box: nil,
+                 fias_id: "32fcb102-2a50-44c9-a00e-806420f448ea",
+                 fias_code: nil,
+                 fias_level: 7,
+                 fias_actuality_state: nil,
+                 kladr_id: "77000000000713400",
+                 geoname_id: nil,
+                 capital_marker: "0",
+                 okato: "45263564000",
+                 oktmo: "45305000",
+                 tax_office: "7718",
+                 tax_office_legal: "7718",
+                 timezone: nil,
+                 geo_lat: 55.8782557,
+                 geo_lon: 37.65372,
+                 beltway_hit: nil,
+                 beltway_distance: nil,
+                 metro: [],
+                 qc_geo: nil,
+                 history_values: ["ул Черненко"]
+               }
+             } = suggestion
     end
   end
 end
