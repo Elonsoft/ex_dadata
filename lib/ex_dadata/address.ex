57f6805a3525a80438c30b8961c0ee3d2839bf31
yunmikun <a.paramonenko@elonsoft.ru>
2021-08-20 19:08:39 +0300

    refactor!: Remove clean_address request
    `ExDadata.Address.clean_address/2` is a duplicate of
    `ExDadata.Address.geocode_address/2` request.

--- lib/ex_dadata/address.ex
+++ lib/ex_dadata/address.ex
@@ -5,38 +5,9 @@ defmodule ExDadata.Address do
   For more information see [docs](https://dadata.ru/api/#address-clean).
   """
 
-  alias __MODULE__.{CleanAddress, SuggestAddress, GeocodeAddress, GeolocateAddress}
+  alias __MODULE__.{GeocodeAddress, GeolocateAddress, SuggestAddress}
   alias ExDadata.Client
   alias ExDadata.HTTPAdapter.Response
-
-  @clean_address_url "https://cleaner.dadata.ru/api/v1/clean/address"
-
-  @doc """
-  Normalize the address.
-
-  See [documentation](https://dadata.ru/api/clean/address/#restrictions)
-  for more information.
-
-  ## Example
-
-      ExDadata.Address.clean_address(client, ["мск сухонска 11/-89"])
-  """
-  def clean_address(client, data) do
-    with {:ok, body} <- do_clean_address(client, data) do
-      {:ok, CleanAddress.new!(body)}
-    end
-  end
-
-  defp do_clean_address(client, data) do
-    headers = Client.to_headers(client)
-    adapter = Client.http_adapter(client)
-
-    case adapter.request(client, :post, @clean_address_url, headers, data, []) do
-      {:ok, %Response{status: 200, body: response}} -> {:ok, response}
-      {:ok, resp} -> {:error, resp}
-      {:error, _} = error -> error
-    end
-  end
 
   @suggest_address_url "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address"
 
--- test/ex_dadata/address_test.exs
+++ test/ex_dadata/address_test.exs
@@ -3,55 +3,6 @@ defmodule ExDadata.AddressTest do
 
   alias ExDadata.{Address, Client}
 
-  describe "clean_address/2" do
-    defmodule MockCleanAddressHTTPAdapter do
-      @behaviour ExDadata.HTTPAdapter
-
-      alias ExDadata.HTTPAdapter.Response
-
-      def request(_, :post, "https://cleaner.dadata.ru/api/v1/clean/address", _, _, _) do
-        body = [
-          %{
-            "source" => "москва сухонская 11",
-            "result" => "г Москва, ул Сухонская, д 11",
-            "postal_code" => "127642",
-            "country" => "Россия",
-            "region" => "Москва",
-            "city_area" => "Северо-восточный",
-            "city_district" => "Северное Медведково",
-            "street" => "Сухонская",
-            "house" => "11",
-            "geo_lat" => "55.8782557",
-            "geo_lon" => "37.65372",
-            "qc_geo" => 0
-          }
-        ]
-
-        {:ok, %Response{status: 200, headers: [], body: body}}
-      end
-    end
-
-    test "returns addresses on success" do
-      client = Client.new(nil, nil, MockCleanAddressHTTPAdapter, nil)
-      assert {:ok, %{items: [item]}} = Address.clean_address(client, [""])
-
-      assert %{
-               source: "москва сухонская 11",
-               result: "г Москва, ул Сухонская, д 11",
-               postal_code: "127642",
-               country: "Россия",
-               region: "Москва",
-               city_area: "Северо-восточный",
-               city_district: "Северное Медведково",
-               street: "Сухонская",
-               house: "11",
-               geo_lat: 55.8782557,
-               geo_lon: 37.65372,
-               qc_geo: 0
-             } = item
-    end
-  end
-
   describe "suggest_address/2" do
     defmodule MockSuggestAddressHTTPAdapter do
       @behaviour ExDadata.HTTPAdapter
